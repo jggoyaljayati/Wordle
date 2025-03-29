511b1411c98050345edfcbf0126e18164267d433
jggoyaljayati <jggoyaljayati@gmail.com>
2025-03-29 15:08:30 -0700

    working color scheme

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -11,10 +11,25 @@ export const Board = () => {
 	const [currWord, setCurrWord] = useState("");
 	const [guessNumber, setGuessNumber] = useState(0);
 
-	useEffect(() => {
-		const handleKeyDown = (event) => {
+	const handleKeyDown = (event) => {
+		if (event.key == "Enter") {
+			if (currWord.length == WORD_LENGTH) {
+				setGuessNumber((prev) => prev + 1);
+				setCurrWord(() => "");
+			}
+			return;
+		} else if (event.key == "Backspace") {
 			setCurrWord((prev) => {
-				console.log(prev + event.key);
+				var newWord = prev.slice(0, -1);
+				setGuessList((prev) => {
+					let newGuess = [...prev]; // Copy the outer array
+					newGuess[guessNumber] = newWord.padEnd(5, " "); // Replace the entire string
+					return newGuess;
+				});
+				return newWord;
+			});
+		} else if (currWord.length < WORD_LENGTH) {
+			setCurrWord((prev) => {
 				var newWord = prev + event.key;
 				setGuessList((prev) => {
 					let newGuess = [...prev]; // Copy the outer array
@@ -23,7 +38,12 @@ export const Board = () => {
 				});
 				return newWord;
 			});
-		};
+		}
+
+		console.log(event.key);
+	};
+
+	useEffect(() => {
 		window.addEventListener("keydown", handleKeyDown);
 		return () => window.removeEventListener("keydown", handleKeyDown);
 	}, [currWord, guessList]);
@@ -42,8 +62,9 @@ export const Board = () => {
 	return (
 		<div className="board">
 			{solution}
-			{guessList.map((guess) => {
-				return <Row word={guess} solution={solution} />;
+			{guessList.map((guess, i) => {
+				let isCurGuess = i == guessNumber;
+				return <Row word={guess} solution={solution} isCurGuess={isCurGuess} />;
 			})}
 			{currWord}
 		</div>
--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,14 +1,16 @@
 import "./../Styles/Row.css";
-export const Row = ({ word, solution }) => {
+export const Row = ({ word, solution, isCurGuess }) => {
 	const tiles = [];
 	for (let i = 0; i < 5; i++) {
 		let classname = "tile ";
-		if (solution && word[i] === solution[i]) {
-			classname += "true";
-		} else if (solution && solution.includes(word[i])) {
-			classname += "partial";
-		} else if (solution && word[i] != " ") {
-			classname += "false";
+		if (isCurGuess == false) {
+			if (solution && word[i] === solution[i]) {
+				classname += "true";
+			} else if (solution && solution.includes(word[i])) {
+				classname += "partial";
+			} else if (solution && word[i] != " ") {
+				classname += "false";
+			}
 		}
 		tiles.push(
 			<div key={i} className={classname}>
